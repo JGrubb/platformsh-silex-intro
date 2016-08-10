064a18d5669525a6702d77bc246dc8f2f3eddbb3
John Grubb <grubb@platform.sh>
2016-08-10 17:00:21 +0200

    init commit

---
+++ app/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jgrubb
+ * Date: 8/10/16
+ * Time: 4:27 PM
+ */
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->get('/hello/{name}', function($name) use($app) {
+    return 'Hello '.$app->escape($name);
+});
+
+$app->run();
