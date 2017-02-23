f21008e5cb5b29583951b5787a0d5bd2f7560088
John Grubb <grubb@johnnygrubb.com>
2017-02-23 11:52:00 -0500

    updating for Composer vid

--- app/index.php
+++ app/index.php
@@ -6,12 +6,12 @@
  * Time: 4:27 PM
  */
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/hello/{name}', function($name) use($app) {
-    return 'Hello '.$app->escape($name);
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return 'Hello ' . $app->escape($name);
 });
 
 $app->run();
