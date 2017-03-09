7eca49c6037e39976e1cd3515707b500179f8f55
John Grubb <grubb@platform.sh>
2017-03-08 20:33:24 -0500

    fixing backup_path

--- backup.php
+++ backup.php
@@ -7,7 +7,7 @@ $psh = new Platformsh\ConfigReader\Config();
 if($psh->isAvailable()) {
     $sql_filename = time() . '.sql';
     $database = $psh->relationships['database'][0];
-    $backup_path = getenv('PLATFORM_DIR') . "/$sql_filename";
+    $backup_path = getenv('PLATFORM_DIR') . "/backups/$sql_filename";
     putenv("PGPASSWORD={$database['password']}");
     exec("pg_dump -U {$database['username']} -h {$database['host']} {$database['path']} > $backup_path");
 
