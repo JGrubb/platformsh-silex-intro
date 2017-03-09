86c7d49e38363363b2b1cb29f3bfaae6fc2a3d90
John Grubb <grubb@platform.sh>
2017-03-08 20:46:19 -0500

    adding logging to backup

--- backup.php
+++ backup.php
@@ -2,23 +2,34 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
 $psh = new Platformsh\ConfigReader\Config();
+$logger = new Logger('backup_logger');
+$logger->pushHandler(new StreamHandler(__DIR__.'/backups/log/backup.log', Logger::DEBUG));
 
-if($psh->isAvailable()) {
-    $sql_filename = date('Y-m-d') . '.sql';
-    $database = $psh->relationships['database'][0];
-    $backup_path = getenv('PLATFORM_DIR') . "/backups/$sql_filename";
-    putenv("PGPASSWORD={$database['password']}");
-    exec("pg_dump -U {$database['username']} -h {$database['host']} {$database['path']} > $backup_path");
+if($psh->isAvailable() && getenv('PLATFORM_BRANCH') === 'master') {
 
-    $s3 = new Aws\S3\S3Client([
-        'version' => 'latest',
-        'region'  => 'us-east-1'
-    ]);
+    try {
+        $sql_filename = date('Y-m-d') . '.sql';
+        $database = $psh->relationships['database'][0];
+        $backup_path = getenv('PLATFORM_DIR') . "/backups/$sql_filename";
+        putenv("PGPASSWORD={$database['password']}");
+        exec("pg_dump -U {$database['username']} -h {$database['host']} {$database['path']} > $backup_path");
 
-    $s3->putObject([
-        'Bucket' => 'ibd-backups',
-        'Key' => "database/$sql_filename",
-        'Body' => fopen($backup_path, 'r')
-    ]);
+        $s3 = new Aws\S3\S3Client([
+            'version' => 'latest',
+            'region'  => 'us-east-1'
+        ]);
+
+        $s3->putObject([
+            'Bucket' => 'ibd-backups',
+            'Key' => "database/$sql_filename",
+            'Body' => fopen($backup_path, 'r')
+        ]);
+        $logger->info("Successfully backed up $sql_filename");
+    } catch (Exception $e) {
+        $logger->error("error: " . $e->getMessage());
+    }
 }
